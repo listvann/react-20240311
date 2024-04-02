5d9361f71b88c1d4e51e154031aa8c05a10e1637
Anna_Lee <listvan2014@yandex.ru>
2024-04-03 00:32:14 +0700

    HW2

--- restaurant/src/components/restaurant/restaurant.jsx
+++ restaurant/src/components/restaurant/restaurant.jsx
@@ -1,4 +1,38 @@
-export const Restaurant =({name,menu,reviews})=>{
+import { Reviews } from '../reviews/component';
+import { Menu } from '../menu/component';
+
+
+export const Restaurant = ({ restaurant }) => {
+    if(!restaurant) {
+        return null
+    }
+    const { name,  menu, reviews} = { restaurant }
+    console.log(restaurant)
+    return (
+        <div> 
+            {name ? name : "NoName"}
+            {/* {restaurant.name} */}
+            {!!menu?.length && (
+               <div>
+                  <h3>Меню</h3>
+                  < Menu menu = {menu}/>
+               </div>
+            )}
+
+            <h3>Отзывы</h3>
+            {reviews?.length ? (
+                <div>
+                    <Reviews reviews = {reviews}/>
+                </div>
+            ) : "Отзывы"}
+        </div>
+    )
+
+};
+
+
+
+/* export const Restaurant =({name,menu,reviews})=>{
 
     return <div>
         <h3>{name}</h3>
@@ -10,4 +44,4 @@ export const Restaurant =({name,menu,reviews})=>{
                 {reviews.map(item=> (<li>{item.text} rating: {item.rating}</li>))}
             </ul>
             </div>
-}
+} */
--- restaurant/src/main.jsx
+++ restaurant/src/main.jsx
@@ -1,6 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {Restaurant} from "./components/restaurant/restaurant";
+import { restaurants } from './constants/mock';
+import { Restaurants } from './restaurants/component';
+import { Layout } from './layout/component';
+import { Restaurant } from './restaurant/component';
+import { Reviews } from './reviews/component';
+import { Review } from './review/component';
+
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <Layout>
+  <div>
+    {restaurants.map((restaurant) => (
+      <Restaurant restaurant = {restaurant}/>
+    ))} 
+  </div>
+</Layout>
+)
+
+/* import {Restaurant} from "./components/restaurant/restaurant";
 import { restaurants } from "../materials/mock";
 
 const rootElement = document.getElementById("root");
@@ -10,3 +29,4 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
   restaurants.map(item => (<Restaurant name={item.name} menu={item.menu} reviews={item.reviews}/>))
 );
+ */
